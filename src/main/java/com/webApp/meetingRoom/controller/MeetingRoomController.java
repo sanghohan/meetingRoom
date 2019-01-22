19001ada3cab7a7ece3f48871b76f206f89141b4
one1000078 <1>
2019-01-22 17:00:50 +0900

    틀짜기..

--- src/main/java/com/webApp/meetingRoom/controller/MeetingRoomController.java
+++ src/main/java/com/webApp/meetingRoom/controller/MeetingRoomController.java
@@ -2,6 +2,7 @@ package com.webApp.meetingRoom.controller;
 
 
 import com.webApp.meetingRoom.vo.RequestReservationRoom;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +13,7 @@ public class MeetingRoomController {
 
     /*회의실 예약 내역 보기 */
     @RequestMapping(value = "/get/reservationList/{month}/{day}/v1", method=RequestMethod.GET)
-    public ModelAndView reservationList() {
+    public ModelAndView reservationList(@PathVariable int month, @PathVariable int day) {
     ModelAndView modelAndView = new ModelAndView();
 
         modelAndView.addObject("test", "hello world!");
